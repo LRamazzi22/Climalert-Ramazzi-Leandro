5106a509a218afdec6199bfc2c0813adfc7769f9
LRamazzi22 <lramazzi@frba.utn.edu.ar>
2026-07-01 00:39:07 -0300

    agrego log al guardar mediciones en el historial

--- src/main/java/ar/edu/utn/frba/ddsi/climalert/repositories/WeatherRepository.java
+++ src/main/java/ar/edu/utn/frba/ddsi/climalert/repositories/WeatherRepository.java
@@ -1,12 +1,14 @@
 package ar.edu.utn.frba.ddsi.climalert.repositories;
 
 import ar.edu.utn.frba.ddsi.climalert.models.WeatherDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@Slf4j
 @Repository
 public class WeatherRepository {
 
@@ -14,6 +16,7 @@ public class WeatherRepository {
 
     public void guardar(WeatherDTO clima) {
         this.historial.add(clima);
+        log.info("Clima guardado. Historial: {} mediciones", historial.size());
     }
 
     public Optional<WeatherDTO> obtenerUltimo() {
